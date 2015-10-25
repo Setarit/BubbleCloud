c240ec63dc33a4cfbef77c02576fd7d784a7f932
JValck <jasper_de_valck@hotmail.com>
2015-10-25 21:11:10 +0100

    View files + opening window

--- java/README.md
+++ java/README.md
@@ -3,4 +3,5 @@ To build this project use Maven.
 There is a built-in profile to run the project. This can be done by typing
 ```mvn -P run install```
 
-
+## Icons
+The iconset can be freely downloaded from [here](http://www.flaticon.com/packs/data-icons/), designed by Freepic.
--- java/src/main/java/com/setarit/bubblecloud/launcher/BubbleCloud.java
+++ java/src/main/java/com/setarit/bubblecloud/launcher/BubbleCloud.java
@@ -1,9 +1,22 @@
 package com.setarit.bubblecloud.launcher;
 
-import javax.swing.JOptionPane;
+import com.setarit.bubblecloud.controller.ConfigurationController;
 
-public class BubbleCloud {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BubbleCloud extends Application{
+	
+	
 	public static void main(String[] args){
-		JOptionPane.showConfirmDialog(null, "bla");
+		launch(args);//launch javafx
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("BubbleCloud - Setarit");
+		ConfigurationController configurationController = new ConfigurationController(primaryStage);
+		configurationController.show();
+	}	
+	
 }
