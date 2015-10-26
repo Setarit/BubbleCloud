bf7a8b3c1afeed5cf5ba91d3a2ffd644920e0c99
JValck <jasper_de_valck@hotmail.com>
2015-10-26 18:39:09 +0100

    Fix logfile name

--- java/src/main/java/com/setarit/bubblecloud/model/settings/ApplicationFileSystem.java
+++ java/src/main/java/com/setarit/bubblecloud/model/settings/ApplicationFileSystem.java
@@ -2,6 +2,8 @@ package com.setarit.bubblecloud.model.settings;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 /**
@@ -14,18 +16,43 @@ public abstract class ApplicationFileSystem {
 	protected final static String USERHOMEFOLDER = System.getProperty("user.home");
 	protected final static String SETARITFOLDERNAME = ".setarit";
 	protected final static String BUBBLECLOUDFOLDERNAME = "bubblecloud";
-	protected final static String SETTINGSFILENAME = "config.json";
+	protected final static String CONFIGURATIONFILENAME = "config.json";
 	protected final static String LOGSFOLDERNAME = "dumps";
 	
-	private File settingsFile, logFile;
+	private File configurationFile, logFile;
 	
 	public ApplicationFileSystem(){
 		initSettingsFile();
+		initLogFile();
+	}
+
+	private void initLogFile() {
+		String logFileName = generateLogFileName();
+		File logFile = new File(USERHOMEFOLDER + File.pathSeparator
+				+ SETARITFOLDERNAME + File.pathSeparator + BUBBLECLOUDFOLDERNAME
+				+ File.pathSeparator + LOGSFOLDERNAME + File.pathSeparator + logFileName);
+		if(!logFile.exists()){
+			logFile.mkdirs();
+			try {
+				logFile.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		this.logFile = logFile;
+	}
+
+	private String generateLogFileName() {
+		Calendar now = Calendar.getInstance();
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy @ HH:mm:ss");
+		String time = dateFormat.format(now.getTime());
+		return time + ".log";
 	}
 
 	private void initSettingsFile() {
-		File settingsFile = new File(SETTINGSFILELOCATION + File.pathSeparator +
-				SETARITFOLDERLOCATION + File.pathSeparator + SETTINGSFOLDERLOCATION);
+		File settingsFile = new File(USERHOMEFOLDER + File.pathSeparator
+				+ SETARITFOLDERNAME + File.pathSeparator + BUBBLECLOUDFOLDERNAME
+				+ File.pathSeparator + CONFIGURATIONFILENAME);
 		if(!settingsFile.exists()){
 			settingsFile.mkdirs();
 			try {
@@ -34,6 +61,6 @@ public abstract class ApplicationFileSystem {
 				e.printStackTrace();
 			}
 		}
-		this.settingsFile = settingsFile;
+		this.configurationFile = settingsFile;
 	}
 }
